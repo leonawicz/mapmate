e22c18fd1d5d84bcf6feb2ab2959b67fd7241cf1
leonawicz <matt.rstat@gmail.com>
2016-10-07 13:08:47 -0600

    Update code for raw bathymetry and borders data processing.

--- data-raw/rawdata.R
+++ data-raw/rawdata.R
@@ -1,16 +1,18 @@
 library(rgdal)
 library(raster)
-library(data.table)
 library(dplyr)
 
 file <- "data-raw/marmap_coord_-180;-90;180;90_res_10.csv"
-nam <- c("long", "lat", "z")
+nam <- c("lon", "lat", "z")
 agg <- 20
 
-d.bath <- read.csv(file) %>%  data.table %>% setnames(nam)
+d.bath <- read.csv(file) %>%  tbl_df %>% setNames(nam)
 r <- raster(extent(-180, 180, -90, 90), res=1/6)
 projection(r) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 r <- setValues(r, d.bath$z)
 
-bathymetry <- aggregate(r, agg) %>% rasterToPoints %>% data.table %>% setnames(nam)
+bathymetry <- aggregate(r, agg) %>% rasterToPoints %>% tbl_df %>% setNames(nam)
 borders <- ggplot2::map_data("world")
+names(borders)[1] <- "lon"
+devtools::use_data(bathymetry, overwrite=TRUE)
+devtools::use_data(borders, overwrite=TRUE)
