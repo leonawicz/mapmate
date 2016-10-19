356760b3829d5c43fa58dab24a5b710543d2b023
leonawicz <matt.rstat@gmail.com>
2016-10-19 13:47:09 -0600

    Add Shiny example app

---
+++ inst/shiny-examples/orthographic/app.R
@@ -0,0 +1,26 @@
+library(shiny)
+data(borders)
+borders <- dplyr::mutate(borders, frameID=1)
+
+ui <- shinyUI(fluidPage(
+   titlePanel("Orthographic projection example"),
+   sidebarLayout(
+      sidebarPanel(
+          h4("Wait a moment for map to load. It may lag behind slider input changes."),
+         sliderInput("lon", "Longitude (lon)", min=-180, max=180, value=0),
+         sliderInput("lat", "Latitude (lat)", min=-90, max=90, value=0),
+         sliderInput("orientation", "Orientation (rotation.axis)", min=0, max=360, value=24)
+      ),
+      mainPanel(
+         plotOutput("orthoMap")
+      )
+   )
+))
+
+server <- shinyServer(function(input, output) {
+   output$orthoMap <- renderPlot({
+     save_map(borders, lon=input$lon, lat=input$lat, col="black", type="maplines", rotation.axis=input$orientation, save.plot=FALSE, return.plot=TRUE)
+   })
+})
+
+shinyApp(ui, server)
