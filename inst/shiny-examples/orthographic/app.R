d11c9e8348db4dc6435dfcfc4e5fe391cd0b904b
leonawicz <matt.rstat@gmail.com>
2016-11-01 11:46:08 -0600

    Update app example

--- inst/shiny-examples/orthographic/app.R
+++ inst/shiny-examples/orthographic/app.R
@@ -1,21 +1,20 @@
 library(shiny)
+library(mapmate)
 data(borders)
 borders <- dplyr::mutate(borders, frameID=1)
 id <- "frameID"
 
 ui <- shinyUI(fluidPage(
-   titlePanel("Orthographic projection example"),
-   sidebarLayout(
-      sidebarPanel(
-          h4("Wait a moment for map to load. It may lag behind slider input changes."),
-         sliderInput("lon", "Longitude (lon)", min=-180, max=180, value=0),
-         sliderInput("lat", "Latitude (lat)", min=-90, max=90, value=0),
-         sliderInput("orientation", "Orientation (rotation.axis)", min=0, max=360, value=24)
-      ),
-      mainPanel(
-         plotOutput("orthoMap")
-      )
-   )
+  fluidRow(
+    column(5,
+      titlePanel("Orthographic projection example"),
+      h4("Wait a moment for map to load. It may lag behind slider input changes."),
+      sliderInput("lon", "Longitude (lon)", min=-180, max=180, value=0, width="100%"),
+      sliderInput("lat", "Latitude (lat)", min=-90, max=90, value=0, width="100%"),
+      sliderInput("orientation", "Orientation (rotation.axis)", min=0, max=360, value=24, width="100%")
+    ),
+    column(7, plotOutput("orthoMap", width="100%"))
+  )
 ))
 
 server <- shinyServer(function(input, output) {
@@ -24,4 +23,4 @@ server <- shinyServer(function(input, output) {
    })
 })
 
-shinyApp(ui, server)
+shinyApp(ui, server, options=list(height=500))
