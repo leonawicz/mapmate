a5c85f9dd737ef3afd77a31bce5c43246d5630b7
leonawicz <matt.rstat@gmail.com>
2016-11-08 14:14:37 -0700

    Fix bugs in unit tests.

--- tests/testthat/test-networks.R
+++ tests/testthat/test-networks.R
@@ -4,28 +4,27 @@ context("networks.R")
 
 data(network)
 distFun <- function(x) 1 - x / max(x) # simple inverse distance weighting
-endpoints <- gc_endpoints(network, "lon", "lat") %>% mutate(Dist_wts=distFun(Dist)) %>%
+d0 <- gc_endpoints(network, "lon", "lat") %>% mutate(Dist_wts=distFun(Dist)) %>%
   sample_n(500, replace=TRUE, weight=(Pop_wts0 + Pop_wts1)/2 + Dist_wts)
-d <- gc_arcs(endpoints, "lon0", "lat0", "lon1", "lat1")
 
 test_that("gc_arcs returns valid output", {
-  expect_error(gc_arcs(d, "lon0", "lat0", "lon1", "lat1", n=c(1,2)), "'n' must have length 1 or length equal to the number of rows in 'data'.")
-  expect_error(gc_arcs(d, "lon0", "lat0", "lon1", "lat1", n=1), "Column 'n' must contain positive integers.")
-  expect_error(gc_arcs(d, "lon0", "lat0", "lon1", "lat1", n="a"), "If 'n' is character, it must refer to a column in 'data'.")
+  expect_error(gc_arcs(d0, "lon0", "lat0", "lon1", "lat1", n=c(1,2)), "'n' must have length 1 or length equal to the number of rows in 'data'.")
+  expect_error(gc_arcs(d0, "lon0", "lat0", "lon1", "lat1", n=0), "Column 'n' must contain positive integers.")
+  expect_error(gc_arcs(d0, "lon0", "lat0", "lon1", "lat1", n="a"), "If 'n' is character, it must refer to a column in 'data'.")
 
-  d <- gc_arcs(slice(d, 1), "lon0", "lat0", "lon1", "lat1", n=10, addStartEnd=FALSE)
+  d <- gc_arcs(slice(d0, 1), "lon0", "lat0", "lon1", "lat1", n=10, addStartEnd=FALSE)
   expect_is(d, "tbl_df")
   expect_equal(nrow(d), 1*(10 + 0))
   expect_equal(ncol(d), 3)
   expect_equal(length(unique(d$group)), 1)
 
-  d <- gc_arcs(slice(d, 1:2), "lon0", "lat0", "lon1", "lat1", n=20)
+  d <- gc_arcs(slice(d0, 1:2), "lon0", "lat0", "lon1", "lat1", n=20)
   expect_is(d, "data.frame")
   expect_equal(nrow(d), 2*(20 + 2))
   expect_equal(ncol(d), 3)
   expect_equal(length(unique(d$group)), 2)
 
-  d <- gc_arcs(mutate(d, n=Dist_wts*15 + 5) %>% slice(1:3), "lon0", "lat0", "lon1", "lat1", n="n")
+  d <- gc_arcs(mutate(d0, n=Dist_wts*15 + 5) %>% slice(1:3), "lon0", "lat0", "lon1", "lat1", n="n")
   expect_is(d, "data.frame")
   expect_equal(ncol(d), 3)
   expect_equal(length(unique(d$group)), 3)
