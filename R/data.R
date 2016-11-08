94bc4a44ffba5949e93c69a9211fe76d21ab2909
leonawicz <matt.rstat@gmail.com>
2016-11-08 13:47:00 -0700

    Add network data set and edit text.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Spatially aggregated example global bathymetry surface
 #'
-#' A bathymetry dataset containing columns of longitude, latitude and depths/elevations relative to sea level,
+#' A bathymetry data set containing columns of longitude, latitude and depths/elevations relative to sea level,
 #' modified from the 10-minute resolution source bathymetry map downloaded via the \code{marmap} package.
 #'
 #' @format A data table with 5832 rows and 3 variables:
@@ -14,7 +14,7 @@
 
 #' Global national political borders
 #'
-#' A dataset containing global national political boundary lines, modified from the world map in the \code{maps} package.
+#' A data set containing global national political boundary lines, modified from the world map in the \code{maps} package.
 #'
 #' @format A data frame with 99338 rows and 6 variables:
 #' \describe{
@@ -30,8 +30,8 @@
 
 #' 2010-2099 global projected annual average temperature anomalies
 #'
-#' A dataset containing low spatial resolution 2010-2099 global projected annual average temperatures.
-#' This is a toy dataset used for example purposes.
+#' A data set containing low spatial resolution 2010-2099 global projected annual average temperatures.
+#' This is a toy data set used for example purposes.
 #' Anomalies are element-wise (cell by cell) delta change from a historical baseline.
 #'
 #' @format A data table with 55080 rows and 4 variables:
@@ -46,10 +46,10 @@
 
 #' 2010-2099 single map grid cell projected monthly average temperatures anomalies
 #'
-#' A dataset containing 2010-2099 projected annual average temperature anomalies for a single low resolution grid cell
-#' taken from a global monthly data set also used as the basis for the \code{annualtemps} dataset in this package.
-#' This is a toy dataset used for example purposes.
-#' This dataset includes data from only one grid cell because it is sufficient for examples but also keeps the size of the data small.
+#' A data set containing 2010-2099 projected annual average temperature anomalies for a single low resolution grid cell
+#' taken from a global monthly data set also used as the basis for the \code{annualtemps} data set in this package.
+#' This is a toy data set used for example purposes.
+#' This data set includes data from only one grid cell because it is sufficient for examples but also keeps the size of the data small.
 #' Anomalies are delta change from a historical baseline.
 #'
 #' @format A data table with 1080 rows and 5 variables:
@@ -62,3 +62,18 @@
 #' }
 #' @source \url{https://github.com/leonawicz/mapmate}
 "monthlytemps"
+
+#' Simulated set of world cities locations and population-based weights
+#'
+#' A data set containing simulated world cities locations and population-based weights.
+#' This is a toy data set used for example purposes.
+#' Sampling and weights are based on the square root of the original populations and are rescaled and rounded.
+#'
+#' @format A data table with 1000 rows and 3 variables:
+#' \describe{
+#'   \item{lon}{longitude}
+#'   \item{lat}{latitude}
+#'   \item{Pop_wts}{population-based weights}
+#' }
+#' @source \url{https://github.com/leonawicz/mapmate}
+"network"
