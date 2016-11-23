952d32d1d51986e21a5b56fbe81ecc2ad1dd29a8
leonawicz <matt.rstat@gmail.com>
2016-11-23 10:31:59 -0700

    Convert to function

--- docs/build_site.R
+++ docs/build_site.R
@@ -1,4 +1,6 @@
-wd <- getwd()
-setwd("C:/github/mapmate/docs")
-rmarkdown::render_site()
-setwd(wd)
+build_site <- function(new_dir, files=NULL){
+  wd <- getwd()
+  setwd(new_dir)
+  if(is.null(files)) rmarkdown::render_site() else rmarkdown::render_site(files)
+  on.exit(setwd(wd))
+}
