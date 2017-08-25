0f001c889194b72a94555846eb8e8a27f6cc3763
leonawicz <matt.rstat@gmail.com>
2017-08-25 17:07:40 -0600

    Delete old files as part of shift to pkgdown.

--- docs/build_site.R
+++
@@ -1,6 +0,0 @@
-build_site <- function(new_dir, files=NULL){
-  wd <- getwd()
-  setwd(new_dir)
-  if(is.null(files)) rmarkdown::render_site() else rmarkdown::render_site(files)
-  on.exit(setwd(wd))
-}
